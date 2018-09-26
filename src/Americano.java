523db44e13d63c84781d065072fb077ac45b55d7
Evgeniya <e.seriakova@bk.ru>
2018-09-26 12:41:20 +0300

    commit 1

---
+++ src/Americano.java
@@ -0,0 +1,16 @@
+public class Americano {
+    private int water;
+    private int beans;
+
+    public Americano(int water, int beans) {
+        this.water = water;
+        this.beans = beans;
+    }
+    public int getWater() {
+        return water;
+    }
+
+    public int getBeans() {
+        return beans;
+    }
+}
---
+++ src/Capuchino.java
@@ -0,0 +1,22 @@
+public class Capuchino {
+    private int water;
+    private int milk;
+    private int beans;
+
+    public Capuchino(int water, int milk, int beans) {
+        this.water = water;
+        this.milk = milk;
+        this.beans = beans;
+    }
+    public int getWater() {
+        return water;
+    }
+
+    public int getMilk() {
+        return milk;
+    }
+
+    public int getBeans() {
+        return beans;
+    }
+}
